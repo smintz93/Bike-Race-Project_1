f2cd78f5cdc06ffac23142f01f84558424187b0e
Sam <sammintz1993@gmail.com>
2018-03-30 12:15:46 -0500

    trying to draw lines

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext("2d");
 
 
+// Middle Line 
+
 ctx.beginPath();
 
 ctx.strokeStyle = "yellow";
@@ -18,8 +20,143 @@ ctx.lineTo(700, 200);
 ctx.stroke();
 
 
-ctx.fill();
 
 ctx.closePath();
+
+// 
+
+// Start Line 
+
+ctx.beginPath();
+
+ctx.strokeStyle = "green";
+
+ctx.moveTo(35, 0);
+ctx.lineTo(35, 400);
+ctx.stroke();
+
+
+
+
+ctx.closePath();
+
+// Finish LIne
+
+ctx.beginPath();
+
+ctx.strokeStyle = "white";
+
+ctx.moveTo(655, 0);
+ctx.lineTo(655, 400);
+ctx.stroke();
+
+
+
+
+ctx.closePath();
+
+// Obstacles // Need 8 total. 4 on each course 
+
+const Obstacles = function () {
+
+
+// First Row
+	ctx.beginPath();
+
+	ctx.strokeStyle = "black"
+
+	ctx.moveTo(157, 100);
+	ctx.lineTo(157, 140);
+	ctx.stroke();
+
+	ctx.closePath();
+
+	ctx.beginPath();
+
+	ctx.strokeStyle = "black"
+
+	ctx.moveTo(157, 300);
+	ctx.lineTo(157, 340);
+	ctx.stroke();
+
+	ctx.closePath();
+
+// Second Row 
+
+	ctx.beginPath();
+
+	ctx.strokeStyle = "black"
+
+	ctx.moveTo(283, 100);
+	ctx.lineTo(283, 140);
+	ctx.stroke();
+
+	ctx.closePath();
+
+	ctx.beginPath();
+
+	ctx.strokeStyle = "black"
+
+	ctx.moveTo(283, 300);
+	ctx.lineTo(283, 340);
+	ctx.stroke();
+
+	ctx.closePath();
+
+// Third Row 
+
+	ctx.beginPath();
+
+	ctx.strokeStyle = "black"
+
+	ctx.moveTo(409, 100);
+	ctx.lineTo(409, 140);
+	ctx.stroke();
+
+	ctx.closePath();
+
+	ctx.beginPath();
+
+	ctx.strokeStyle = "black"
+
+	ctx.moveTo(409, 300);
+	ctx.lineTo(409, 340);
+	ctx.stroke();
+
+	ctx.closePath();
+
+// Fourth Row 	
+
+	ctx.beginPath();
+
+	ctx.strokeStyle = "black"
+
+	ctx.moveTo(560, 100);
+	ctx.lineTo(560, 140);
+	ctx.stroke();
+
+	ctx.closePath();
+
+	ctx.beginPath();
+
+	ctx.strokeStyle = "black"
+
+	ctx.moveTo(560, 300);
+	ctx.lineTo(560, 340);
+	ctx.stroke();
+
+	ctx.closePath();
+
+
+
+
+}
+
+Obstacles();
+
+
+
+
+
 
 
